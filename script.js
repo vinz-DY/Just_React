23fb63bc247b83c2a39571bc61cc6fedd4748b31
vinz-DY <vincentducamp259@hotmail.com>
2023-10-05 18:27:48 +0200

    ajout du refresh igloo

--- script.js
+++ script.js
@@ -185,3 +185,11 @@ inputKey.addEventListener('keydown', function postNewCardInput(event) {
     });
   })
 })
+
+const reloadtButton = document.querySelector("#reload");
+// Reload everything:
+function reload() {
+    reload = location.reload();
+}
+// Event listeners for reload
+reloadButton.addEventListener("click", reload, false);
