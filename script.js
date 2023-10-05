3a3067dcfe3bbe6d558e52251b958139f5ce15c3
iNaeRoZ <naeroz@outlook.fr>
2023-10-05 09:18:35 +0200

    leago

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
-const buttons = document.querySelectorAll('.fa-heart-circle-check')
+var buttons = document.querySelectorAll('.fa-heart-circle-check');
 let count = 5;
+
+let comments = 2;
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
@@ -8,14 +10,174 @@ buttons.forEach(button => {
     const id = +button.id.split("_").pop()// ["coeur","1"]
     const likecounternum = document.getElementById(`counter_${id}`);
    
-  
-   
     if (button.className.includes("blueheart")) {
-     likecounternum.textContent = +likecounternum.innerText +1;
-     button.classList.replace("blueheart","redheart")
+      likecounternum.textContent = +likecounternum.innerText +1;
+      button.classList.replace("blueheart","redheart")
     } else {
       likecounternum.textContent = +likecounternum.innerText -1;
-     button.classList.replace("redheart","blueheart")
-}
+      button.classList.replace("redheart","blueheart")
+    }
   });
+})
+
+const postNewCard = () => {
+  comments++;
+  let text = document.getElementById("textCard").value;
+  // Etape 1 = Sélection de la div
+  let div = document.getElementById("feed-container");
+
+  let newArticle = document.createElement("article");
+  newArticle.setAttribute("class", "feed-card-1")
+
+  let newCard = `<div class="card-header" >
+     
+
+      <div class="feed-card-header2" >
+          <img src="image/vincent.jpg" alt="photo de l'utilisateur" class="photo-profil-1">
+          <div class="feed-card-username">
+              <h1>Vinz D-Y</h1>
+              <p class="date"> 27 septembre 2023</p>
+          </div>
+      </div>
+       <div class="like-count" >
+        <i class="fa-sharp fa-solid fa-heart-circle-bolt"></i><!-- icon like count -->
+        <p id="counter_${comments}" class="like-count-num" >5</p>
+       </div>
+  </div>
+  <div>
+      <p class="fdc-content" >
+          ${text}
+      </p>
+  </div>
+  <div class="footer-card" >
+      <div class="left-buttons" >
+          <i id="coeur_${comments}" class="fa-solid fa-heart-circle-check blueheart" ></i> <!--icon like-->
+      </div>
+      <div class="left-b"> 
+          <i class="fa-solid fa-id-card"></i><!--icon cardfolio-->
+      </div>
+      <div class="right-buttons" >
+          <i class="fa-regular fa-comments"></i> <!--icon like-->
+      </div>
+      <div class="right-b"> 
+          <i class="fa-solid fa-square-share-nodes"></i><!--icon cardfolio-->
+      </div>
+      
+      
+  </div>`;
+
+  newArticle.innerHTML = newCard;
+
+  // Etape 2 = Récupération des enfants de div
+  let enfantsDiv = div.children;
+
+  enfantsDiv = Array.from(enfantsDiv);
+
+  // Etape 4 = Réinsérer les enfants dans la div
+  for(let element of enfantsDiv){
+    div.appendChild(element);
+  }
+
+  div.prepend(newArticle)
+  const buttons = document.querySelectorAll('.fa-heart-circle-check');
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      // Incrémente le compteurcon
+      const id = +button.id.split("_").pop()// ["coeur","1"]
+      const likecounternum = document.getElementById(`counter_${id}`);
+     
+      if (button.className.includes("blueheart")) {
+        likecounternum.textContent = +likecounternum.innerText +1;
+        button.classList.replace("blueheart","redheart")
+      } else {
+        likecounternum.textContent = +likecounternum.innerText -1;
+        button.classList.replace("redheart","blueheart")
+      }
+    });
   })
+}
+
+let inputKey = document.getElementById("textCard");
+
+inputKey.addEventListener('keydown', function postNewCardInput(event) {
+  
+  if(event.key != "Enter"){
+    return;
+  }
+
+  comments++;
+
+  let text = document.getElementById("textCard").value;
+  // Etape 1 = Sélection de la div
+  let div = document.getElementById("feed-container");
+
+  let newArticle = document.createElement("article");
+  newArticle.setAttribute("class", "feed-card-1")
+
+  let newCard = `<div class="card-header" >
+     
+
+      <div class="feed-card-header2" >
+          <img src="image/vincent.jpg" alt="photo de l'utilisateur" class="photo-profil-1">
+          <div class="feed-card-username">
+              <h1>Vinz D-Y</h1>
+              <p class="date"> 27 septembre 2023</p>
+          </div>
+      </div>
+       <div class="like-count" >
+        <i class="fa-sharp fa-solid fa-heart-circle-bolt"></i><!-- icon like count -->
+        <p id="counter_${comments}" class="like-count-num" >5</p>
+       </div>
+  </div>
+  <div>
+      <p class="fdc-content" >
+          ${text}
+      </p>
+  </div>
+  <div class="footer-card" >
+      <div class="left-buttons" >
+          <i id="coeur_${comments}" class="fa-solid fa-heart-circle-check blueheart" ></i> <!--icon like-->
+      </div>
+      <div class="left-b"> 
+          <i class="fa-solid fa-id-card"></i><!--icon cardfolio-->
+      </div>
+      <div class="right-buttons" >
+          <i class="fa-regular fa-comments"></i> <!--icon like-->
+      </div>
+      <div class="right-b"> 
+          <i class="fa-solid fa-square-share-nodes"></i><!--icon cardfolio-->
+      </div>
+      
+      
+  </div>`;
+
+  newArticle.innerHTML = newCard;
+
+  // Etape 2 = Récupération des enfants de div
+  let enfantsDiv = div.children;
+
+  enfantsDiv = Array.from(enfantsDiv);
+
+  // Etape 4 = Réinsérer les enfants dans la div
+  for(let element of enfantsDiv){
+    div.appendChild(element);
+  }
+
+  div.prepend(newArticle)
+  const buttons = document.querySelectorAll('.fa-heart-circle-check');
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      // Incrémente le compteurcon
+      const id = +button.id.split("_").pop()// ["coeur","1"]
+      const likecounternum = document.getElementById(`counter_${id}`);
+     
+      if (button.className.includes("blueheart")) {
+        likecounternum.textContent = +likecounternum.innerText +1;
+        button.classList.replace("blueheart","redheart")
+      } else {
+        likecounternum.textContent = +likecounternum.innerText -1;
+        button.classList.replace("redheart","blueheart")
+      }
+    });
+  })
+})
