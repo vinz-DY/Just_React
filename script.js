ba1f1d8f1a1d31f865c3064e77f0482bf3d8a056
vinz-DY <vincentducamp259@hotmail.com>
2023-10-06 10:40:00 +0200

    modifi bouton pour eviter ajouter double clic coeur

--- script.js
+++ script.js
@@ -33,73 +33,56 @@ const postNewCard = () => {
   let date = new Date().toDateString();
 
   let newCard = `<div class="card-header" >
-
-     
-
-      <div class="feed-card-header2" >
-          <img src="image/vincent.jpg" alt="photo de l'utilisateur" class="photo-profil-1">
-          <div class="feed-card-username">
-              <h1>Vinz D-Y</h1>
-              <p class="date">${date}</p>
-          </div>
+    <div class="feed-card-header2" >
+      <img src="image/vincent.jpg" alt="photo de l'utilisateur" class="photo-profil-1">
+      <div class="feed-card-username">
+        <h1>Vinz D-Y</h1>
+        <p class="date">${date}</p>
       </div>
-       <div class="like-count" >
-        <i class="fa-sharp fa-solid fa-heart-circle-bolt"></i><!-- icon like count -->
-        <p id="counter_${comments}" class="like-count-num" ></p>
-       </div>
+    </div>
+    <div class="like-count" >
+      <i class="fa-sharp fa-solid fa-heart-circle-bolt"></i><!-- icon like count -->
+      <p id="counter_${comments}" class="like-count-num" ></p>
+    </div>
   </div>
   <div>
-      <p class="fdc-content" >
-          ${textCard.value}
-      </p>
+    <p class="fdc-content" >
+      ${textCard.value}
+    </p>
   </div>
   <div class="footer-card" >
-      <div class="left-buttons" >
-          <i id="coeur_${comments}" class="fa-solid fa-heart-circle-check blueheart" ></i> <!--icon like-->
-      </div>
-      <div class="left-b"> 
-          <i class="fa-solid fa-id-card"></i><!--icon cardfolio-->
-      </div>
-      <div class="right-buttons" >
-          <i class="fa-regular fa-comments"></i> <!--icon like-->
-      </div>
-      <div class="right-b"> 
-          <i class="fa-solid fa-square-share-nodes"></i><!--icon cardfolio-->
-      </div>
-      
-      
+    <div class="left-buttons" >
+      <i id="coeur_${comments}" class="fa-solid fa-heart-circle-check blueheart" ></i> <!--icon like-->
+    </div>
+    <div class="left-b"> 
+      <i class="fa-solid fa-id-card"></i><!--icon cardfolio-->
+    </div>
+    <div class="right-buttons" >
+      <i class="fa-regular fa-comments"></i> <!--icon like-->
+    </div>
+    <div class="right-b"> 
+      <i class="fa-solid fa-square-share-nodes"></i><!--icon cardfolio-->
+    </div>
   </div>`;
 
   newArticle.innerHTML = newCard;
-  console.log(textCard);
-  textCard.value = "";
-  // Etape 2 = Récupération des enfants de div
-  let enfantsDiv = div.children;
-
-  enfantsDiv = Array.from(enfantsDiv);
-
-  // Etape 4 = Réinsérer les enfants dans la div
-  for(let element of enfantsDiv){
-    div.appendChild(element);
-  }
-
   div.prepend(newArticle)
-  const buttons = document.querySelectorAll('.fa-heart-circle-check');
-  buttons.forEach(button => {
-    button.addEventListener('click', () => {
-      // Incrémente le compteurcon
-      const id = +button.id.split("_").pop()// ["coeur","1"]
-      const likecounternum = document.getElementById(`counter_${id}`);
-     
-      if (button.className.includes("blueheart")) {
-        likecounternum.textContent = +likecounternum.innerText +1;
-        button.classList.replace("blueheart","redheart")
-      } else {
-        likecounternum.textContent = +likecounternum.innerText -1;
-        button.classList.replace("redheart","blueheart")
-      }
-    });
-  })
+  textCard.value = "";
+
+  const button = document.querySelector('.fa-heart-circle-check');
+  button.addEventListener('click', () => {
+    // Incrémente le compteurcon
+    const id = +button.id.split("_").pop()// ["coeur","1"]
+    const likecounternum = document.getElementById(`counter_${id}`);
+    console.log(id);
+    if (button.className.includes("blueheart")) {
+      likecounternum.textContent = +likecounternum.innerText +1;
+      button.classList.replace("blueheart","redheart")
+    } else {
+      likecounternum.textContent = +likecounternum.innerText -1;
+      button.classList.replace("redheart","blueheart")
+    }
+  });
 }
 
 let inputKey = document.getElementById("textCard");
